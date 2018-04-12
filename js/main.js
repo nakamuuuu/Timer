e5bb372c7c01e59b0f04b5c71595cad38fb1e514
yuri <nakamura@weatherlight.co.jp>
2018-04-12 19:06:10 +0900

    add count down and stop timer

---
+++ js/main.js
@@ -0,0 +1,48 @@
+(function() {
+  'use strict';
+
+  var timer = document.getElementById('timer');
+  var min = document.getElementById('min');
+  var sec = document.getElementById('sec');
+  var reset = document.getElementById('reset');
+  var start = document.getElementById('start');
+
+  var startTime;
+  var timeLeft;
+  var timeToCountDown = 4 * 1000;
+  var timerId;
+
+  function updateTimer(t) {
+    var d = new Date(t);
+    var m = d.getMinutes();
+    var s = d.getSeconds();
+    var ms = d.getMilliseconds();
+    m = ('0' + m).slice(-2);
+    s = ('0' + s).slice(-2);
+    ms = ('00' + ms).slice(-3);
+
+    timer.textContent = m + ':' + s + '.' + ms;
+  }
+
+  function countDown() {
+    timerId = setTimeout(function() {
+      // var elapsedTime = Date.now() - startTime;
+      // timeLeft = timeToCountDown - elapsedTime;
+      timeLeft = timeToCountDown - (Date.now() - startTime);
+      if (timeLeft < 0) {
+        clearTimeout(timerId);
+        timeLeft = 0;
+        timeToCountDown = 0;
+        updateTimer(timeLeft);
+        return;
+      }
+      updateTimer(timeLeft);
+      countDown();
+    }, 10);
+  }
+
+  start.addEventListener('click', function() {
+    startTime = Date.now();
+    countDown();
+  });
+})();
