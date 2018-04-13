1ab2000d998215370ab21eeaee52df7aa4c357d8
yuri <nakamura@weatherlight.co.jp>
2018-04-13 17:15:50 +0900

    finished all task

--- js/main.js
+++ js/main.js
@@ -9,19 +9,22 @@
 
   var startTime;
   var timeLeft;
-  var timeToCountDown = 4 * 1000;
+  var timeToCountDown = 0;
   var timerId;
+  var isRunning = false;
 
   function updateTimer(t) {
     var d = new Date(t);
     var m = d.getMinutes();
     var s = d.getSeconds();
     var ms = d.getMilliseconds();
+    var timerString;
     m = ('0' + m).slice(-2);
     s = ('0' + s).slice(-2);
     ms = ('00' + ms).slice(-3);
-
-    timer.textContent = m + ':' + s + '.' + ms;
+    timerString = m + ':' + s + '.' + ms;
+    timer.textContent = timerString;
+    document.title = timerString;
   }
 
   function countDown() {
@@ -30,6 +33,8 @@
       // timeLeft = timeToCountDown - elapsedTime;
       timeLeft = timeToCountDown - (Date.now() - startTime);
       if (timeLeft < 0) {
+        isRunning = false;
+        start.textContent = 'Start';
         clearTimeout(timerId);
         timeLeft = 0;
         timeToCountDown = 0;
@@ -42,7 +47,43 @@
   }
 
   start.addEventListener('click', function() {
-    startTime = Date.now();
-    countDown();
+    if (isRunning === false) {
+      isRunning = true;
+      start.textContent = 'Stop';
+      startTime = Date.now();
+      countDown();
+    } else {
+      isRunning = false;
+      start.textContent = 'Start';
+      timeToCountDown = timeLeft;
+      clearTimeout(timerId);
+    }
+  });
+
+  min.addEventListener('click', function() {
+    if (isRunning === true) {
+      return;
+    }
+    timeToCountDown += 60 * 1000;
+    if (timeToCountDown >= 60 * 60 * 1000) {
+      timeToCountDown = 0;
+    }
+    updateTimer(timeToCountDown);
+  });
+
+  sec.addEventListener('click', function() {
+    if (isRunning === true) {
+      return;
+    }
+    timeToCountDown += 1000;
+    if (timeToCountDown >= 60 * 60 * 1000) {
+      timeToCountDown = 0;
+    }
+    updateTimer(timeToCountDown);
+  });
+
+  reset.addEventListener('click', function() {
+    timeToCountDown = 0;
+    updateTimer(timeToCountDown);
   });
 })();
